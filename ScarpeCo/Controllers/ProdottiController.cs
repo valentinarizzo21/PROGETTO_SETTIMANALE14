642b72c7dfc6e7f1cf234282cf541fd5aef56496
rizzo <rizzo@SOFIA>
2025-02-21 15:04:51 +0100

    Aggiungere i file di progetto.

---
+++ ScarpeCo/Controllers/ProdottiController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScarpeCo.Models;
+using System.Collections.Generic;
+
+public class ProdottiController : Controller
+{
+    public IActionResult Index()
+    {
+        List<Prodotto> prodotti = ProdottiRepository.GetProdotti();
+        return View(prodotti);
+    }
+
+    public IActionResult Dettagli(int id)
+    {
+        Prodotto prodotto = ProdottiRepository.GetProdottoById(id);
+        if (prodotto == null)
+        {
+            return NotFound();
+        }
+        return View(prodotto);
+    }
+
+    public IActionResult Modifica(int id)
+    {
+        Prodotto prodotto = ProdottiRepository.GetProdottoById(id);
+        if (prodotto == null)
+        {
+            return NotFound();
+        }
+        return View(prodotto);
+    }
+
+    [HttpPost]
+    public IActionResult SalvaModifica(Prodotto modificato)
+    {
+        Prodotto prodottoEsistente = ProdottiRepository.GetProdottoById(modificato.Id);
+        if (prodottoEsistente == null)
+        {
+            return NotFound();
+        }
+
+        prodottoEsistente.Nome = modificato.Nome;
+        prodottoEsistente.Prezzo = modificato.Prezzo;
+        prodottoEsistente.Descrizione = modificato.Descrizione;
+        prodottoEsistente.ImmagineCopertina = modificato.ImmagineCopertina;
+
+        return RedirectToAction("Index");
+    }
+}
---
+++ ScarpeCo/Models/Prodotto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public class Prodotto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public decimal Prezzo { get; set; }
+    public string Descrizione { get; set; }
+    public string ImmagineCopertina { get; set; }
+    public List<string> ImmaginiAggiuntive { get; set; }
+}
